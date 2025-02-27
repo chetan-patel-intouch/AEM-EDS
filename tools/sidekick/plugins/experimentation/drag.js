95c4a7208e741b33327477d411b1869232fc5dd9
Chris Millar <cmillar@adobe.com>
2025-02-26 22:21:14 -0700

    EXP - Beginnings

---
+++ tools/sidekick/plugins/experimentation/drag.js
@@ -0,0 +1,36 @@
+export default function makeDraggable(el) {
+  let pos1 = 0;
+  let pos2 = 0;
+  let pos3 = 0;
+  let pos4 = 0;
+
+  function closeDragElement() {
+    // Remove event listeners when done dragging
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  function elementDrag(e) {
+    e.preventDefault();
+    // Calculate new position
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // Set parent element's new position
+    el.style.top = `${el.offsetTop - pos2}px`;
+    el.style.left = `${el.offsetLeft - pos1}px`;
+  }
+
+  function dragMouseDown(e) {
+    e.preventDefault();
+    // Get mouse position at startup
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // Add event listeners for mouse movement and release
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  el.querySelector('#aem-sidekick-exp-title').onmousedown = dragMouseDown;
+}
--- tools/sidekick/plugins/experimentation/experimentation.js
+++ tools/sidekick/plugins/experimentation/experimentation.js
@@ -1,3 +1,35 @@
+import { loadCSS } from '../../../../scripts/aem.js';
+import makeDraggable from './drag.js';
+
+await loadCSS(`${import.meta.url.replace('js', 'css')}`);
+
+const MAIN_SELECTOR = 'aem-sidekick-exp';
+
 export default async function runExp() {
-    console.log('hi');
+  // If the palette exists, hide it.
+  let palette = document.querySelector(`#${MAIN_SELECTOR}`);
+  if (palette && palette.classList.contains('is-visible')) {
+    palette.classList.remove('is-visible');
+    return;
+  }
+
+  // Create a net new palette
+  palette = document.createElement('div');
+  palette.id = MAIN_SELECTOR;
+
+  // Title
+  const title = document.createElement('p');
+  title.innerText = 'Experimentation';
+  title.id = `${MAIN_SELECTOR}-title`;
+
+  // Iframe
+  const iframe = document.createElement('iframe');
+  iframe.src = 'https://da.live/app/aemsites/da-block-collection/tools/sidekick/plugins/experimentation/mfe/mfe?ref=local';
+  iframe.allow = 'clipboard-write *';
+
+  // Append
+  palette.append(title, iframe);
+
+  makeDraggable(palette);
+  document.body.append(palette);
 }
