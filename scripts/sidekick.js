3b3def5a2043a08adcdd18921480a70a7288f7ae
Chris Millar <cmillar@adobe.com>
2025-03-01 12:56:32 -0700

    Setup for lanes of dev

--- scripts/sidekick.js
+++ scripts/sidekick.js
@@ -1,4 +1,11 @@
-import runExp from 'http://localhost:6456/nx/public/plugins/exp/exp.js';
+// Production
+// import runExp from 'http://da.live/nx/public/plugins/exp/exp.js';
+
+// Branch dev
+import runExp from 'http://exp--nexter--da-sites.aem.live/nx/public/plugins/exp/exp.js';
+
+// Local dev
+// import runExp from 'http://localhost:6456/nx/public/plugins/exp/exp.js';
 
 runExp();
 
