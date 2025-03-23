2387b99675811f2668b7f3cb9d7706481fa660c2
Chris Millar <cmillar@adobe.com>
2025-03-23 07:33:20 -0600

    WIP

--- scripts/context.js
+++ scripts/context.js
@@ -1,6 +1,16 @@
 import { loadCSS } from './aem.js';
 
+const SECTION_SELECTOR = '.section';
 const EDITABLE_SELECTORS = 'h1, h2, p';
+
+const overlay = document.createElement('div');
+overlay.className = 'nx-overlay';
+
+const editor = document.createElement('div');
+editor.className = 'nx-editor';
+overlay.append(editor);
+
+document.body.append(overlay);
 
 function handleEditable(editable) {
   const childEdits = editable.querySelectorAll(EDITABLE_SELECTORS);
@@ -17,12 +27,31 @@ function handleEditable(editable) {
 
     // Set the editable attr and set focus
     editable.setAttribute('contenteditable', true);
-    setTimeout(() => { editable.focus(); }, 150);
+    setTimeout(() => { editable.focus(); }, 100);
   });
 }
 
-export default async function init(html) {
+function handleSection(section) {
+  section.addEventListener('mouseenter', (e) => {
+    const rect = section.getBoundingClientRect();
+
+    const { scrollTop, scrollLeft } = document.documentElement;
+    const top = scrollTop + rect.top;
+    const left = scrollLeft + rect.left;
+
+    // Position relative to the document
+    editor.style = `left: ${left}px; top: ${top}px; width: ${rect.width}px; height: ${rect.height}px`;
+  });
+  section.addEventListener('mouseleave', (e) => {
+    editor.style = '';
+  });
+}
+
+export default async function init() {
   await loadCSS('/styles/context.css');
+  const sections = document.body.querySelectorAll(SECTION_SELECTOR);
+  sections.forEach((section) => { handleSection(section); });
+
   const editables = document.body.querySelectorAll(EDITABLE_SELECTORS);
   editables.forEach((editable) => { handleEditable(editable); });
 }
